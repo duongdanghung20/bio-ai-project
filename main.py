422b83803e6c713c2bf29839a34f74aaece58965
Duong Dang Hung <hungdd.bi10-073@st.usth.edu.vn>
2022-05-23 13:12:29 +0200

    Completed neuroevolution with feedforward NN

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import neat
 import sys
 import random
 import os
+import pickle
 import math
 from pygame.math import Vector2
+from sklearn import config_context
+
+
+NUMGEN = 50
 
 
 WIN_WIDTH = 900
@@ -43,6 +48,8 @@ VEC_DOWN = Vector2(0, 1)
 VEC_LEFT = Vector2(-1, 0)
 VEC_RIGHT = Vector2(1, 0)
 
+global apple_pos
+
 class Apple:
     IMG = pygame.transform.scale(APPLE_IMG, (BIN_WIDTH, BIN_HEIGHT))
     def __init__(self, x, y):
@@ -50,6 +57,9 @@ class Apple:
         self.y = y
         self.pos = Vector2(self.x, self.y)
         self.eaten = False
+
+        global apple_pos
+        apple_pos = self.pos
 
 
     def draw(self, win):
@@ -61,8 +71,10 @@ class Snake:
     BODY_IMG = pygame.transform.scale(SNAKE_BODY_IMG, (BIN_WIDTH, BIN_HEIGHT)).convert_alpha()
     BODY_TURN_IMG = pygame.transform.scale(SNAKE_BODY_TURN_IMG, (BIN_WIDTH, BIN_HEIGHT)).convert_alpha()
     TAIL_IMG = pygame.transform.scale(SNAKE_TAIL_IMG, (BIN_WIDTH, BIN_HEIGHT)).convert_alpha()
+    INIT_HEAD_X = round(NO_BINS_X / 5)
+    INIT_HEAD_Y = round(NO_BINS_Y / 2)
     def __init__(self):
-        self.body = [Vector2(7, 10), Vector2(6, 10), Vector2(5, 10)]
+        self.body = [Vector2(self.INIT_HEAD_X, self.INIT_HEAD_Y), Vector2(self.INIT_HEAD_X - 1, self.INIT_HEAD_Y), Vector2(self.INIT_HEAD_X - 2, self.INIT_HEAD_Y)]
         self.direction = Vector2(1, 0)
         self.new_block = False
         self.head_img = self.HEAD_IMG
@@ -155,6 +167,140 @@ class Snake:
     def add_block(self):
         self.new_block = True
 
+    def sensor_1(self):
+        global apple_pos
+        temp = Vector2(self.body[0].x, self.body[0].y)
+        dist_to_wall = temp.y 
+        dist_to_apple = math.inf
+        dist_to_self = math.inf
+        counter = 0
+        while temp.y > 0:
+            counter += 1
+            temp += VEC_UP
+            if temp == apple_pos:
+                dist_to_apple = counter
+            if temp in self.body:
+                dist_to_self = counter
+        return dist_to_wall, dist_to_apple, dist_to_self
+
+    def sensor_2(self):
+        global apple_pos
+        temp = Vector2(self.body[0].x, self.body[0].y)
+        dist_to_wall = 0
+        dist_to_apple = math.inf
+        dist_to_self = math.inf
+        counter = 0
+        while temp.y > 0 and temp.x < NO_BINS_X - 1:
+            counter += 1
+            temp += VEC_UP + VEC_RIGHT
+            if temp == apple_pos:
+                dist_to_apple = counter
+            if temp in self.body:
+                dist_to_self = counter
+        dist_to_wall = counter
+        return dist_to_wall, dist_to_apple, dist_to_self
+
+    def sensor_3(self):
+        global apple_pos
+        temp = Vector2(self.body[0].x, self.body[0].y)
+        dist_to_wall = NO_BINS_X - temp.x - 1
+        dist_to_apple = math.inf
+        dist_to_self = math.inf
+        counter = 0
+        while temp.x < NO_BINS_X - 1:
+            counter += 1
+            temp += VEC_RIGHT
+            if temp == apple_pos:
+                dist_to_apple = counter
+            if temp in self.body:
+                dist_to_self = counter
+        return dist_to_wall, dist_to_apple, dist_to_self
+
+    def sensor_4(self):
+        global apple_pos
+        temp = Vector2(self.body[0].x, self.body[0].y)
+        dist_to_wall = 0
+        dist_to_apple = math.inf
+        dist_to_self = math.inf
+        counter = 0
+        while temp.y < NO_BINS_Y - 1 and temp.x < NO_BINS_X - 1:
+            counter += 1
+            temp += VEC_RIGHT + VEC_DOWN
+            if temp == apple_pos:
+                dist_to_apple = counter
+            if temp in self.body:
+                dist_to_self = counter
+        dist_to_wall = counter
+        return dist_to_wall, dist_to_apple, dist_to_self
+
+    def sensor_5(self):
+        global apple_pos
+        temp = Vector2(self.body[0].x, self.body[0].y)
+        dist_to_wall = NO_BINS_Y - temp.y - 1
+        dist_to_apple = math.inf
+        dist_to_self = math.inf
+        counter = 0
+        while temp.y < NO_BINS_Y - 1:
+            counter += 1
+            temp += VEC_DOWN
+            if temp == apple_pos:
+                dist_to_apple = counter
+            if temp in self.body:
+                dist_to_self = counter
+        return dist_to_wall, dist_to_apple, dist_to_self
+
+    def sensor_6(self):
+        global apple_pos
+        temp = Vector2(self.body[0].x, self.body[0].y)
+        dist_to_wall = 0
+        dist_to_apple = math.inf
+        dist_to_self = math.inf
+        counter = 0
+        while temp.y < NO_BINS_Y - 1 and temp.x > 0:
+            counter += 1
+            temp += VEC_DOWN + VEC_LEFT
+            if temp == apple_pos:
+                dist_to_apple = counter
+            if temp in self.body:
+                dist_to_self = counter
+        dist_to_wall = counter
+        return dist_to_wall, dist_to_apple, dist_to_self
+    
+    def sensor_7(self):
+        global apple_pos
+        temp = Vector2(self.body[0].x, self.body[0].y)
+        dist_to_wall = temp.x
+        dist_to_apple = math.inf
+        dist_to_self = math.inf
+        counter = 0
+        while temp.x > 0:
+            counter += 1
+            temp += VEC_LEFT
+            if temp == apple_pos:
+                dist_to_apple = counter
+            if temp in self.body:
+                dist_to_self = counter
+        return dist_to_wall, dist_to_apple, dist_to_self
+
+    def sensor_8(self):
+        global apple_pos
+        temp = Vector2(self.body[0].x, self.body[0].y)
+        dist_to_wall = 0
+        dist_to_apple = math.inf
+        dist_to_self = math.inf
+        counter = 0
+        while temp.y > 0 and temp.x > 0:
+            counter += 1
+            temp += VEC_UP + VEC_LEFT
+            if temp == apple_pos:
+                dist_to_apple = counter
+            if temp in self.body:
+                dist_to_self = counter
+        dist_to_wall = counter
+        return dist_to_wall, dist_to_apple, dist_to_self
+
+
+
 def draw_bg(win):
     # Draw the background
     color_dark_green = True
@@ -194,62 +340,136 @@ def fail(snake):
         return True
     return False
 
-def main():
-    score = 0
-    snake = Snake()
-    init_apple_x = random.randint(0, NO_BINS_X - 1)
-    init_apple_y = random.randint(0, NO_BINS_Y - 1)
-    apples = [Apple(init_apple_x, init_apple_y)]
+def main(genomes, config):
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        snake = Snake()
+        g.fitness = 0
 
-    run = True
+        init_apple_x = random.randint(0, NO_BINS_X - 1)
+        init_apple_y = random.randint(0, NO_BINS_Y - 1)
+        apples = [Apple(init_apple_x, init_apple_y)]
 
-    while run:
-        clock.tick(15)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        score = 0
+        run = True
+
+        while run:
+            clock.tick(120)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                    sys.exit()
+
+            apple_ind = 0
+            if len(apples) > 1 and snake.body[0] == apples[0].pos:
+                apple_ind = 1
+
+            g.fitness += 0.1
+            input = (*snake.sensor_1(), *snake.sensor_2(), *snake.sensor_3(), *snake.sensor_4(), *snake.sensor_5(), *snake.sensor_6(), *snake.sensor_7(), *snake.sensor_8())
+            output = net.activate(input)
+            if output[0] == max(output):
+                snake.turn_down()
+            if output[1] == max(output):
+                snake.turn_left()
+            if output[2] == max(output):
+                snake.turn_right()
+            if output[3] == max(output):
+                snake.turn_up()
+            
+            snake.move()
+
+            apples_rmv = []
+            add_apple = False
+            for apple in apples:
+                if not apple.eaten and apple.pos == snake.body[0]:
+                    apple.eaten = True
+                    add_apple = True
+                    apples_rmv.append(apple)
+
+            if add_apple:
+                score += 1
+                g.fitness += 5
+                while True:
+                    new_apple_x , new_apple_y = (random.randint(0, NO_BINS_X - 1), random.randint(0, NO_BINS_Y - 1))
+                    if (new_apple_x != apple.x or new_apple_y != apple.y) and (Vector2(new_apple_x, new_apple_y) not in snake.body):
+                        apples.append(Apple(new_apple_x, new_apple_y))
+                        break
+                snake.add_block()
+
+            for apple in apples_rmv:
+                apples.remove(apple)
+
+            if fail(snake):
+                g.fitness -= 2
+                # genomes.remove(g)
                 run = False
+            
+            draw_window(win, snake, apples, score)
 
-        snake.move()
+# def main():
+#     snake = Snake()
 
-        keys = pygame.key.get_pressed()
-        current_snake_direction = snake.direction
-        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and current_snake_direction != VEC_LEFT and snake.moved:
-            snake.turn_right()
-        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and current_snake_direction != VEC_RIGHT and snake.moved:
-            snake.turn_left()
-        if (keys[pygame.K_UP] or keys[pygame.K_w]) and current_snake_direction != VEC_DOWN and snake.moved:
-            snake.turn_up()
-        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and current_snake_direction != VEC_UP and snake.moved:
-            snake.turn_down()
+#     init_apple_x = random.randint(0, NO_BINS_X - 1)
+#     init_apple_y = random.randint(0, NO_BINS_Y - 1)
+#     apples = [Apple(init_apple_x, init_apple_y)]
 
-        apples_rmv = []
-        add_apple = False
-        for apple in apples:
-            if not apple.eaten and apple.pos == snake.body[0]:
-                apple.eaten = True
-                add_apple = True
-                apples_rmv.append(apple)
+#     score = 0
+#     run = True
 
-        if add_apple:
-            score += 1
-            while True:
-                new_apple_x , new_apple_y = (random.randint(0, NO_BINS_X - 1), random.randint(0, NO_BINS_Y - 1))
-                if (new_apple_x != apple.x or new_apple_y != apple.y) and (Vector2(new_apple_x, new_apple_y) not in snake.body):
-                    apples.append(Apple(new_apple_x, new_apple_y))
-                    break
-            snake.add_block()
+#     while run:
+#         clock.tick(15)
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 run = False
+#                 pygame.quit()
+#                 sys.exit()
 
-        for apple in apples_rmv:
-            apples.remove(apple)
+#         snake.move()
+#         # TODO
 
-        if fail(snake):
-            print(f"Score = {score}")
-            run = False
+#         apples_rmv = []
+#         add_apple = False
+#         for apple in apples:
+#             if not apple.eaten and apple.pos == snake.body[0]:
+#                 apple.eaten = True
+#                 add_apple = True
+#                 apples_rmv.append(apple)
+
+#         if add_apple:
+#             score += 1
+#             while True:
+#                 new_apple_x , new_apple_y = (random.randint(0, NO_BINS_X - 1), random.randint(0, NO_BINS_Y - 1))
+#                 if (new_apple_x != apple.x or new_apple_y != apple.y) and (Vector2(new_apple_x, new_apple_y) not in snake.body):
+#                     apples.append(Apple(new_apple_x, new_apple_y))
+#                     break
+#             snake.add_block()
+
+#         for apple in apples_rmv:
+#             apples.remove(apple)
+
+#         print(apple_pos)
+
+#         if fail(snake):
+#             print(f"Score = {score}")
+#             run = False
         
-        draw_window(win, snake, apples, score)
+#         draw_window(win, snake, apples, score)
 
-    pygame.quit()
-    sys.exit()
+
+def run(config_path):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, NUMGEN)
+    with open("best_bird_ge.p", "wb") as best_g:
+        pickle.dump(winner, best_g)
 
 if __name__ == "__main__":
-    main()
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)
